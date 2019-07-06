ff8c8b30eb4f4486443198e17e8419a461f4b88c
Rob Paik <robpaik@gmail.com>
2019-07-06 21:27:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,19 +5,22 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  my_hash = {:name => 
+  my_hash = {:name => "Rob Paik"}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  return pioneer {:name => "Grace Hopper"}
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  hash = {:id => 3}
 end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+  
 end
 
 def read_from_hash(hash, key)
