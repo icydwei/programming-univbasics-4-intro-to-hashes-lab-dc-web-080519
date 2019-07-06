483ebc7421a0d5a3acfac81e44f355842b7fe05e
Rob Paik <robpaik@gmail.com>
2019-07-06 21:43:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,7 @@ end
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
   if hash[key]
-    return hash
+    return hash[key]
   else
     return nil
   end
