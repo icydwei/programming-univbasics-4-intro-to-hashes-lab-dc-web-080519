a3468c0404da80fae8ecb24b05b2500e3d22496e
Rob Paik <robpaik@gmail.com>
2019-07-06 21:41:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,8 +20,9 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  my_hash_creator[key] = value
-  my_hash_creator
+  hash = {}
+  hash[key] = value
+  hash
 end
 
 
